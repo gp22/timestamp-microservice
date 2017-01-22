d149b0052ca5edb2009cf4b2b11584756006b2e3
gp22 <gp22@tutanota.com>
2017-01-22 04:16:48 +0000

    add basic styling and timestamp experimentation

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var express = require('express');
+var timestamp = require('@augmt/timestamp-microservice');
 var app = express();
+
+// console.log(timestamp);
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
     res.render('home');
+});
+
+app.get('/:datevalue', function(req, res) {
+    var datevalue = req.params.datevalue;
+    console.log('datevalue is ' + datevalue);
 });
 
 app.listen(3000, function() {
