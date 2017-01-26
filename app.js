779fe4f5f98f414400d9ebeccdf22cceeada55ae
gp22 <gp22@tutanota.com>
2017-01-26 06:58:16 +0000

    add date value parsing and validation

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var moment = require('moment');
-// var timestamp = require('@augmt/timestamp-microservice');
 var app = express();
-
-// console.log(timestamp);
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -14,16 +11,36 @@ app.get('/', function(req, res) {
 
 app.get('/:datevalue', function(req, res) {
     var datevalue = req.params.datevalue;
-    // check for valid date in datevalue
-    if (moment(datevalue).isValid()) {
-      res.send(moment(datevalue).format('MMMM Do YYYY'));
-    }
-
     var response = {
-      "unix": 1450137600,
-      "natural": "December 15, 2015"
+        "unix": null,
+        "natural": null
     };
-    // res.send(moment.unix(datevalue).isValid());
+    // check for valid natural language date in datevalue
+    if (moment(datevalue,
+       ['MMMM DD YYYY', 'MMM DD YYYY',
+        'MM DD YYYY', 'MM DD YY',
+        'M D YYYY', 'M D YY',
+        'YYYY M D', 'YYYY MM DD',
+        'MM-DD-YYYY', 'MM-DD-YY',
+        'M-D-YYYY', 'M-D-YY',
+        'YYYY-M-D', 'YYYY-MM-DD'], true)
+       .isValid()) {
+        var unix = Number((moment(datevalue).format('X')));
+        var natural = (moment(datevalue).format('MMMM DD, YYYY'));
+        response["unix"] = unix;
+        response["natural"] = natural;
+        res.send(response);
+    // check for valid unix timestamp in datevalue
+    } else if (moment(datevalue, 'X', true).isValid()) {
+        var unix = Number((moment.unix(datevalue).format('X')));
+        var natural = (moment.unix(datevalue).format('MMMM DD, YYYY'));
+        response["unix"] = unix;
+        response["natural"] = natural;
+        res.send(response);
+    // send default response if no valid datevalue found
+    } else {
+        res.send(response);
+    }
 });
 
 app.listen(3000, function() {
